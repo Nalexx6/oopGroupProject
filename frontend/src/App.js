e331c8109c071eead22d2867d8ed92b469477e1f
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2021-06-13 22:41:55 +0300

    Minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Feed from "./pages/Feed"
 import Profile from "./pages/Profile"
 import Push_project from "./pages/Push_project"
+import Review from "./pages/Review";
 
 function App() {
   return (
-    <Push_project/>
+    <Review/>
   );
 }
 
