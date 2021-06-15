feca95fd9c587f005c19c6d44f880b10f49f370d
N_alexx <nikita2002oleksienko@gmail.com>
2021-06-15 22:44:02 +0300

    Auth imported

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Push_project from "./pages/Push_project"
 import Project from "./pages/Project";
 import Sign_in from "./pages/Sign_in"
 import Sign_up from './pages/Sign_up';
+import AuthContext from './context/AuthContext';
+import {useAuth} from "./hooks/auth-hook";
 
 import {
   BrowserRouter as Router,
@@ -15,29 +17,38 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const { login, logout, userId } = useAuth();
   return (
-    <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Sign_in />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/project">
-            <Project />
-          </Route>
-          <Route path="/feed">
-            <Feed />
-          </Route>
-          <Route path="/sign_up">
-            <Sign_up />
-          </Route>
-          <Route path="/push_project">
-            <Push_project />
-          </Route>
-        </Switch>
-    </Router>
+    <AuthContext.Provider
+        value={{
+          userId: userId,
+          login: login,
+          logout: logout
+        }}
+    >
+      <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Sign_in />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/project">
+              <Project />
+            </Route>
+            <Route path="/feed">
+              <Feed />
+            </Route>
+            <Route path="/sign_up">
+              <Sign_up />
+            </Route>
+            <Route path="/push_project">
+              <Push_project />
+            </Route>
+          </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
