08c08e7923c5490712145e57e9ce7a1e15ba8571
fivard <bernadadmytryi@gmail.com>
2021-06-15 23:33:33 +0300

    Deleted nick and short description

--- frontend/src/pages/Push_project.js
+++ frontend/src/pages/Push_project.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Header.css"
 import "./Push_project.css"
 import Header from "./Header";
+import { Link } from 'react-router-dom';
 
 const Push_project = () => {
     return (
@@ -10,18 +11,16 @@ const Push_project = () => {
             <div className={"main"}>
                 <div className={"main-content"}>
                     <div className={"main-content-left-column"}>
-                        <label className={"input-label"}>Project title</label>
-                        <input className={"input-data"} type="text" />
-
-                        <label className={"input-label"}>Nick</label>
-                        <input className={"input-data"} type="text" />
-
-                        <label className={"input-label"}>Short description</label>
-                        <input className={"input-data"} type="text" />
-
-                        <div className={"add-project-button"}>
-                            <p className={"add-project-text"}>Push project</p>
+                        <div>
+                            <label className={"input-label"}>Project title</label>
+                            <input className={"input-data"} type="text" />
                         </div>
+
+                        <Link to="/profile">
+                            <div className={"project-button"}>
+                                <p className={"add-project-text"}>Push project</p>
+                            </div>
+                        </Link>
                     </div>
                     <div className={"main-content-right-column"}>
                         <textarea className={"text-area"} placeholder="Add your code here"/>
