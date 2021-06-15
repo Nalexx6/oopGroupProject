314996c78d93c855680a71c3728ba2bd62d0cd48
N_alexx <nikita2002oleksienko@gmail.com>
2021-06-15 09:18:15 +0300

    Service cors fixed

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const projectRoutes = require('./routes/project-routes');
 const usersRoutes = require('./routes/users-routes');
 const reviewRoutes = require('./routes/review-routes');
+const cors = require('cors')
 
 const HttpError = require('./models/http-error');
 const app = express();
@@ -11,9 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors()) // Use this after the variable declaration
 app.use('/api/projects', projectRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/reviews', reviewRoutes)
+
+
 
 app.use((req, res, next) => {
     next(new HttpError('Could not find this route'), 404)
--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -1,5 +1,11 @@
-export function getProjectsForUser() {
-    alert("LOL111")
-    return fetch('http://localhost:5000/api/projects/user/60c7a4eb35e6652af83b85dc')
-        .then(data => data.json())
+export async function fetchProjectsForUser() {
+    let response = await fetch('http://localhost:5000/api/projects/user/60c7a4eb35e6652af83b85dc', {
+        headers: {
+            'Access-Control-Allow-Origin' : 'origin-list',
+            'Content-Type': 'application/json'
+        },
+    });
+    let result = await response.json();
+    console.log(result)
+    return result;
 }
