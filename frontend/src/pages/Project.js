ec4b72a9d19458ef2a9f0192cd14bcfaaf90b315
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2021-06-15 19:09:42 +0300

    Update Project.js

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -6,28 +6,50 @@ import Header from "./Header";
 
 import {fetchProject, fetchProjectsForUser, fetchReview, fetchUser} from "../services/service";
 
-const LeftBarGeneration = ({project}) => {
+// const woteUpProject = ({project}) => {
+//     project.rating++;
+// }
+//
+// const woteDownProject = ({project}) => {
+//     project.rating++;
+// }
+
+const handleSubmit = (project, inputValue, setProject) => {
+    project.review_data.push(inputValue);
+    setProject(prev => ({
+        ...prev,
+        review_data:{
+            ...review_data, inputValue
+        }
+    }))
+}
+
+
+
+const LeftBarGeneration = ({project, setProject}) => {
     return(
         <div className={"left-bar"}>
-            <Button style={{width: 40}} variant="success">^</Button>
+            <Button style={{width: 40}} variant="success" >^</Button>
             <p className={"review-text"}>{project.rating}</p>
-            <Button style={{width: 40}} variant="danger">v</Button>
+            <Button style={{width: 40}} variant="danger" >v</Button>
         </div>
     )
 
 }
 
-const RightBarGeneration = ({project}) => {
+const RightBarGeneration = ({project, setProject}) => {
+    const [inputValue, setInputValue] = useState("");
+
     return(
         <div className={"right-bar"}>
             <p className={"review-text"}>Author: {project.user.login} | {project.title} </p>
             <div className={"review-text-div"}><textarea className={"text-area"} placeholder={project.code}/></div>
             <div className={"comment-div"}>
-                <input className={"input-comment"} type="text" />
-                <Button style={{textAlign: "left"}}  variant="info">Publish</Button>
+                 <input className={"input-comment"} value={inputValue} onChange={(event) => {setInputValue(event.target.value)}} type="text" />
+                 <Button style={{textAlign: "left"}}  variant="info" onClick={() => handleSubmit(project, inputValue, setProject)} >Publish</Button>
             </div>
             {
-                project.review_data.forEach(review =>
+                project.review_data.map(review =>
                     <div className={"comment-div"}>
                         {review.content}
                     </div>)
@@ -41,11 +63,11 @@ const Project = () => {
     const [loading, setLoading] = useState(true)
         useEffect ( () => {
         const getProjectForUser = async () => {
-            const _project = await fetchProject("60c7aa024579762e58699c04")
+            const _project = await fetchProject("60c7a52035e6652af83b85df")
             _project.user = await fetchUser(_project.creator);
-            _project.review_data = []
+            _project.review_data = [_project.reviews.length]
             for(let i = 0;i < _project.reviews.length; i++) {
-                _project.review_data[i] = await fetchReview(_project.review[i]);
+                _project.review_data[i] = await fetchReview(_project.reviews[i]);
             }
             setProject(_project)
             setLoading(false)
@@ -72,8 +94,8 @@ const Project = () => {
                     </div>
                     :
                     <div className={"row"}>
-                        <LeftBarGeneration project = {project}/>
-                        <RightBarGeneration  project = {project}/>
+                        <LeftBarGeneration project = {project} setProject = {setProject}/>
+                        <RightBarGeneration  project = {project} setProject = {setProject}/>
                     </div>
                 }
             </div>
