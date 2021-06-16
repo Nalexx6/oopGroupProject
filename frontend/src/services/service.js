cf5fcf3d30ea99538d812a58128cd740920c3635
N_alexx <nikita2002oleksienko@gmail.com>
2021-06-16 12:26:57 +0300

    projects for user parametrized

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -1,5 +1,5 @@
-export async function fetchProjectsForUser() {
-    let response = await fetch('http://localhost:5000/api/projects/user/60c7a4eb35e6652af83b85dc')
+export async function fetchProjectsForUser(id) {
+    let response = await fetch('http://localhost:5000/api/projects/user/' + id)
     let result = await response.json();
     console.log(result)
     return result.projects;
@@ -19,7 +19,7 @@ export async function fetchProject(id) {
     return result.project;
 }
 
-export async function fetchUser(id) {
+export async function fetchUserById(id) {
     let response = await fetch('http://localhost:5000/api/users/'+ id)
     let result = await response.json();
     console.log(result)
