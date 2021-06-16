15deff94f6321e45dc51f05060686430ae6789a6
fivard <bernadadmytryi@gmail.com>
2021-06-16 15:15:17 +0300

    Sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 function App() {
--- frontend/src/pages/Sign_up.js
+++ frontend/src/pages/Sign_up.js
@@ -1,13 +1,42 @@
 import React, {useEffect, useState, useContext} from 'react';
 import Button from 'react-bootstrap/Button';
 import { AuthContext } from '../context/AuthContext';
-import Header from "./Header";
 import "./Sign_in.css"
 import "./Push_project.css"
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { signUpUser } from '../services/service';
+
+const handleSignUp = async (inputLogin, inputEmail, inputPassword, auth, history,
+                            setInputLogin, setInputEmail, setInputPassword) => {
+    let user = {
+        login: inputLogin,
+        email: inputEmail,
+        password: inputPassword,
+        image: "123"
+    }
+
+    let loggedUser = await signUpUser(user);
+
+
+    if(loggedUser != null) {
+        auth.login(loggedUser.id);
+        history.push("/profile");
+    } else {
+        setInputLogin("");
+        setInputEmail("");
+        setInputPassword("");
+    }
+}
 
 
 const Sign_up = () => {
+    const [inputLogin, setInputLogin] = useState("");
+    const [inputEmail, setInputEmail] = useState("");
+    const [inputPassword, setInputPassword] = useState("");
+
+    const auth = useContext(AuthContext);
+    let history = useHistory();
+
     return (
     <div>
         <div className="main1">
@@ -16,17 +45,23 @@ const Sign_up = () => {
                     <p className="text">
                         Login
                     </p>
-                    <input className={"input-data"} type="text" />
+                    <input className={"input-data"} value={inputLogin}
+                            onChange={(event) => {setInputLogin(event.target.value)}} type="text" />
                     <p className="text">
                         Email
                     </p>
-                    <input className={"input-data"} type="text" />
+                    <input className={"input-data"} value={inputEmail}
+                            onChange={(event) => {setInputEmail(event.target.value)}} type="text" />
                     <p className="text">
                         Password
                     </p>
-                    <input className={"input-data"} type="text" />
+                    <input className={"input-data"} value={inputPassword}
+                            onChange={(event) => {setInputPassword(event.target.value)}} type="text" />
+
                     <div className="submit">
-                        <Link to="/"><Button className="button" variant="success" >Sign Up</Button></Link>
+                        <Button className="button" variant="success" onClick={() => 
+                                    handleSignUp(inputLogin, inputEmail, inputPassword, auth, history,
+                                                setInputLogin, setInputEmail, setInputPassword)}>Sign Up</Button>
                     </div>
                 </div>
             </div>
