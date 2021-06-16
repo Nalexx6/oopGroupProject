42445d5c474ce7d8e4e88a4a2f7b34d117cd6292
Maryna <52110829+zolottareva@users.noreply.github.com>
2021-06-16 15:27:38 +0300

    small changes to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    login: { type: String, required: true},
+    login: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
     image: { type: String, required: true },
